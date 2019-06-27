fbcfda5968b9840d04745662f309594d323a2ca4
Cedric Orejola <cedricorejola@gmail.com>
2019-06-26 17:53:02 -0700

    created base homework files

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+// Import ORM
+var orm = require('../config/orm.js');
+
+// create burgers model using ORM
+
+var burger = {
+
+    all: function () {
+        // orm.all("burgers", function (res){
+        //     cb(res);
+        // });
+    },
+
+    create: function () {
+        // orm.create("burgers", function(res){
+        //     cb(res);
+        // })
+    },
+
+    update: function () {
+        // orm.update("burgers", function(res){
+        //     cb(res)
+        // });
+    }
+
+};
